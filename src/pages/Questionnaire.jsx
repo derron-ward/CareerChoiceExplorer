e9e922840b8b4d75d912425d89992689645c5d91
Tristan Besser <tristan.besser58@gmail.com>
2023-04-12 00:21:25 -0400

    Questionnaire 50% complete

--- src/pages/Questionnaire.jsx
+++ src/pages/Questionnaire.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import './Questionnaire.css'
 
 function Questionnaire() {
   return (
-    <div>Questionnaire</div>
+    <div id='questionnaire-page'>
+      <div id='left-section'>
+        <h1 id='question'>
+          Question
+        </h1>
+        <p id='description'>
+          Lorem Ipsum
+        </p>
+      </div>
+      <div id='right-section'>
+        <div id='progress-bar'>
+          <div class='progress'>
+            <div class="progress-done"></div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
