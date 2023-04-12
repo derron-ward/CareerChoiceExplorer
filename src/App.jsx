17a41694f0fc390d8903bc0b87b00d53d26459a5
Derron Ward <derronward.dev@gmail.com>
2023-04-12 00:12:50 -0400

    Implemented Sticky Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,27 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
 
+import Navbar from './components/Navbar'
+
 import Home from './pages/Home'
 import Questionnaire from './pages/Questionnaire'
 import Salaries from './pages/Salaries'
 import Industries from './pages/Industries'
 import Events from './pages/Events'
+import Profile from './pages/Profile'
 
 function App() {
   return (
     <div className="App">
         <Router>
+            <Navbar />
             <Routes>
                 <Route path='/' element={<Home />} />
                 <Route path='/questionnaire' element={<Questionnaire />} />
                 <Route path='/salaries' element={<Salaries />} />
                 <Route path='/industries' element={<Industries />} />
                 <Route path='/events' element={<Events />} />
+                <Route path='/profile' element={<Profile />} />
             </Routes>
         </Router>
     </div>
