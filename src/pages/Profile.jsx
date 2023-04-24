430ede1d0bfc34c1fb02350817b67060ea327e92
Derron Ward <derronward.dev@gmail.com>
2023-04-23 21:27:51 -0400

    fixed no page load

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -3,31 +3,32 @@ import './Profile.css'
 
 function Profile() {
   return (
-    <div>Profile</div>
+    <div>
+      <div className="ChangePassword">
+        <form onSubmit={handleChangeClick}>
+          <FormGroup bsSize="large" controlId="oldPassword">
+            <FormLabel>Old Password</FormLabel>
+          </FormGroup>
+          <FormGroup bsSize="large" controlId="password">
+            <FormLabel>New Password</FormLabel>
+          </FormGroup>
+          <FormGroup bsSize="large" controlId="confirmPassword">
+            <FormLabel>Confirm Password</FormLabel>
+          </FormGroup>
+          <LoaderButton
+            block
+            type="submit"
+            bsSize="large"
+            disabled={!validateForm()}
+            isLoading={isChanging}
+          >
+            Change Password
+          </LoaderButton>
+        </form>
+      </div>
+    </div>
   )
 }
 
-  <div className="ChangePassword">
-    <form onSubmit={handleChangeClick}>
-      <FormGroup bsSize="large" controlId="oldPassword">
-        <FormLabel>Old Password</FormLabel>
-      </FormGroup>
-      <FormGroup bsSize="large" controlId="password">
-        <FormLabel>New Password</FormLabel>
-      </FormGroup>
-      <FormGroup bsSize="large" controlId="confirmPassword">
-        <FormLabel>Confirm Password</FormLabel>
-      </FormGroup>
-      <LoaderButton
-        block
-        type="submit"
-        bsSize="large"
-        disabled={!validateForm()}
-        isLoading={isChanging}
-      >
-        Change Password
-      </LoaderButton>
-    </form>
-  </div>
 
 export default Profile
