2612916c5f6645d1976e5c2869a178da572c5670
Ramone928 <ramonefields@Ramones-MacBook-Pro.local>
2023-04-23 20:48:24 -0400

    password form

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
+import './Profile.css'
 
 function Profile() {
   return (
     <div>Profile</div>
   )
 }
+
+  <div className="ChangePassword">
+    <form onSubmit={handleChangeClick}>
+      <FormGroup bsSize="large" controlId="oldPassword">
+        <FormLabel>Old Password</FormLabel>
+      </FormGroup>
+      <FormGroup bsSize="large" controlId="password">
+        <FormLabel>New Password</FormLabel>
+      </FormGroup>
+      <FormGroup bsSize="large" controlId="confirmPassword">
+        <FormLabel>Confirm Password</FormLabel>
+      </FormGroup>
+      <LoaderButton
+        block
+        type="submit"
+        bsSize="large"
+        disabled={!validateForm()}
+        isLoading={isChanging}
+      >
+        Change Password
+      </LoaderButton>
+    </form>
+  </div>
 
 export default Profile
